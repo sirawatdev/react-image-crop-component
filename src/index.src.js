ac59575d1fbd8fa036fd84f4c4ce67326d43a0f0
Sirawat <Sirawat.Israradej@scb.co.th>
2019-10-25 23:01:30 +0700

    fix

--- src/index.src.js
+++ src/index.src.js
@@ -119,7 +119,7 @@ class ReactImageCrop extends Component {
             resize_top_block = this.refs.RICC_crop_block_top_resize,
             resize_bottom_block = this.refs.RICC_crop_block_bottom_resize,
             mousePosX = e.clientX || Math.round(e.touches[0].clientX),
-            mousePosY = e.clientY+e.currentTarget.getBoundingClientRect().top || Math.round(e.touches[0].clientY+e.currentTarget.getBoundingClientRect().top);
+            mousePosY = e.clientY+document.getElementById('scrollParag').scrollTop || Math.round(e.touches[0].clientY+document.getElementById('scrollParag').scrollTop);
 
         if(!this.state.activeCropBlock){
             // CREATE CROP BLOCK FUNCTION
