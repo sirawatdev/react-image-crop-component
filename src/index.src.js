45e5d0f84d21c346c66a5610c78b79f03020cbe8
Sirawatdev <sirawat.israradej@scb.co.th>
2021-08-05 22:28:50 +0700

    remove scroll top

--- src/index.src.js
+++ src/index.src.js
@@ -120,7 +120,7 @@ class ReactImageCrop extends Component {
             resize_top_block = this.refs.RICC_crop_block_top_resize,
             resize_bottom_block = this.refs.RICC_crop_block_bottom_resize,
             window_size = this.state.windowsWidth,
-            mousePosX = e.pageX+((window_size-document.body.clientWidth)/clientX) || Math.round(e.touches[0].pageX+((window_size-document.body.clientWidth)/clientX)),
+            mousePosX = e.pageX+((window_size-document.body.clientWidth)/e.clientX) || Math.round(e.touches[0].pageX+((window_size-document.body.clientWidth)/e.clientX)),
             mousePosY = e.clientY+document.body.scrollTop || Math.round(e.touches[0].clientY+document.body.scrollTop);
 
         if(!this.state.activeCropBlock){
@@ -212,7 +212,7 @@ class ReactImageCrop extends Component {
                 crop_block = this.refs.RICC_crop_block,
                 preview_block = this.refs.RICC_crop_preview,
                 window_size = this.state.windowsWidth,
-                mousePosX = e.pageX+((window_size-document.body.clientWidth)/clientX) || Math.round(e.touches[0].pageX+((window_size-document.body.clientWidth)/clientX)),
+                mousePosX = e.pageX+((window_size-document.body.clientWidth)/e.clientX) || Math.round(e.touches[0].pageX+((window_size-document.body.clientWidth)/e.clientX)),
                 mousePosY = e.pageY+document.body.scrollTop || Math.round(e.touches[0].clientY+document.body.scrollTop);
 
             if(!this.state.activeCropBlock){
