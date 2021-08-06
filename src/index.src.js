373bae58e76652b6a3294c59ac58b1df6e2ea03a
Sirawatdev <sirawat.israradej@scb.co.th>
2021-08-06 11:44:14 +0700

    remove scroll top

--- src/index.src.js
+++ src/index.src.js
@@ -119,9 +119,9 @@ class ReactImageCrop extends Component {
             resize_right_block = this.refs.RICC_crop_block_right_resize,
             resize_top_block = this.refs.RICC_crop_block_top_resize,
             resize_bottom_block = this.refs.RICC_crop_block_bottom_resize,
-            rect = e.getBoundingClientRect(),
-            mousePosX = e.pageX-rect.left,
-            mousePosY = e.pageY-rect.top;
+            rect = e.target.getBoundingClientRect(),
+            mousePosX = e.clientX-rect.left,
+            mousePosY = e.clientY-rect.top;
             // window_size = this.state.windowsWidth,
             // mousePosX = e.clientX+((window_size-document.body.clientWidth)/6) || Math.round(e.touches[0].clientX+((window_size-document.body.clientWidth)/6)),
             // mousePosY = e.clientY+document.body.scrollTop || Math.round(e.touches[0].clientY+document.body.scrollTop);
@@ -214,9 +214,9 @@ class ReactImageCrop extends Component {
             let main_block = this.refs.RICC_main_container,
                 crop_block = this.refs.RICC_crop_block,
                 preview_block = this.refs.RICC_crop_preview,
-                rect = e.getBoundingClientRect(),
-                mousePosX = e.pageX-rect.left,
-                mousePosY = e.pageY-rect.top;
+                rect = e.target.getBoundingClientRect(),
+                mousePosX = e.clientX-rect.left,
+                mousePosY = e.clientY-rect.top;
                 // window_size = this.state.windowsWidth,
                 // mousePosX = e.pageX+((window_size-document.body.clientWidth)/6) || Math.round(e.touches[0].pageX+((window_size-document.body.clientWidth)/6)),
                 // mousePosY = e.pageY+document.body.scrollTop || Math.round(e.touches[0].clientY+document.body.scrollTop);
